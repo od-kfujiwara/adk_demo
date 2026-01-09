208a64b6775f7a7c36e954028ffa0da8acc687d3
od-kfujiwara <kfujiwara@opendoor.co.jp>
2026-01-09 17:33:22 +0900

    日付を答えるエージェントを追加

--- agents/agent.py
+++ agents/agent.py
@@ -2,6 +2,7 @@ from google.adk.agents import Agent
 from agents.instruction import INSTRUCTION
 from agents.coffee_agent.agent import coffee_agent
 from agents.tea_agent.agent import tea_agent
+from agents.general_agent.agent import general_agent
 from agents.callbacks import get_token, log_token_usage
 
 root_agent = Agent(
@@ -9,7 +10,7 @@ root_agent = Agent(
     description="飲み物の案内人として、コーヒーや紅茶の専門エージェントに適切にルーティングします",
     model="gemini-2.0-flash",
     instruction=INSTRUCTION,
-    sub_agents=[coffee_agent, tea_agent],
+    sub_agents=[coffee_agent, tea_agent, general_agent],
     after_agent_callback=log_token_usage,
-    after_model_callback=get_token
+    after_model_callback=get_token,
 )
